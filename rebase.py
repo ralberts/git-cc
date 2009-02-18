4addc7d9a3402bb013b0496f22238278d71c22b2
Alex Ward <awarddev2@gmail.com>
2009-02-18 08:38:18 -0600

    Fix for Index Out of Bounds Error due to missing branch
    Previously, if the user was missing a branch from their gitcc config file,
    an 'Index Out of Bounds Error' occured. This commit trys to inform the user
    of the problem before barfing.

--- rebase.py
+++ rebase.py
@@ -221,7 +221,9 @@ class Uncataloged(Changeset):
                 date = cc_exec(['describe', '-fmt', '%d', dir])
                 def f(s):
                     return s[0] == 'checkinversion' and s[1] < date and filterBranches(s[2])
-                version = list(filter(f, list(map(lambda x: x.split('|'), history.split('\n')))))[0][2]
+                versions = list(filter(f, list(map(lambda x: x.split('|'), history.split('\n')))))
+                if len(versions) == 0:
+                    raise Exception("It appears that you may be missing a branch (or have a mis-spelling) in the includes section of your gitcc config file.")  
                 self._add(added, version.strip())
 
 class Version(object):
