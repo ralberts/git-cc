5a427f35566c2dba44dc3a47c15e6285e127a21d
Alex Ward <awarddev2@gmail.com>
2009-02-24 08:46:41 -0600

    Fixing issue with sync -- incorrect paths were being used for the 'to' file

--- sync.py
+++ sync.py
@@ -5,15 +5,17 @@ import os, shutil, stat
 from os.path import join, abspath
 from fnmatch import fnmatch
 
+
 def main(glob):
     base = abspath(CC_DIR)
     for i in cfg.getList('include', '.'):
-        for (dirpath, dirnames, filenames) in os.walk(join(CC_DIR, i)):
+        for (dirpath, dirnames, filenames) in os.walk(buildPath([CC_DIR, i])):
             reldir = dirpath[len(base):]
             for file in filenames:
                 if fnmatch(file, glob):
-                    newFile = join(GIT_DIR, reldir, file)
-                    debug('Copying %s' % newFile)
+                    newFile = buildPath([GIT_DIR, reldir, file])
+                    fromFile = buildPath([dirpath,file])
+                    print('Copying',fromFile,"to",newFile)
                     mkdirs(newFile)
-                    shutil.copy(join(dirpath, file), newFile)
+                    shutil.copy(fromFile, newFile)
                     os.chmod(newFile, stat.S_IWRITE)
