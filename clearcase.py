0ff0522ac4b7b47bdfd86b050ead673c33252b9e
Alex Ward <awarddev2@gmail.com>
2009-02-24 08:43:52 -0600

    Fixing iss with getCurrentVersions - history was not being split on line endings and thus incorrect paths where being used

--- clearcase.py
+++ clearcase.py
@@ -80,7 +80,7 @@ class Clearcase:
         if elm.path in cls.currentVersions:
             return cls.currentVersions[elm.path]
         if os.path.exists(elm.getAbsolutePath()):
-            split = cls._exec(['ls','-short',elm.path]).split("@@")
+            split = cls._exec(['ls','-short',elm.path]).splitlines()[0].split("@@")
             if len(split) < 2:
             	return None
             cls.currentVersions[split[0]] = Element(split[0],split[1])
