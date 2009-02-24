c64bbf4abc897ea0f3c2bcee8d81eedec010ecd1
Alex Ward <awarddev2@gmail.com>
2009-02-24 14:35:41 -0600

    Added call to move the checkin tag after history import

--- merge.py
+++ merge.py
@@ -33,7 +33,8 @@ def main(stash=False, dry_run=False, lshistory=False, load=None, since=None):
     if not (stash or dry_run or lshistory):
         git.checkPristine()
     acquire.main(since=since,load=load,lshistory=lshistory,dry_run=dry_run)
-    
+
     print('Performing merge...')
     git.checkout(branch)
     git.merge(CC_TAG)
+    tag(CI_TAG, CC_TAG)    
