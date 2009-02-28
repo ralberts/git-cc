af73d81e33b2aa5d113a20804d946a81922e6579
Ryan Alberts <ralberts@ptc.com>
2009-02-27 18:01:36 -0600

    Redmine link...

--- daemon.py
+++ daemon.py
@@ -22,7 +22,7 @@ ARGS = {
 CHECKIN_BRANCH=cfg.get("checkin_branch","")
 ADMIN_EMAIL=cfg.get("admin_email","")
 SLEEP_TIME=cfg.get("sleep_time",5)
-
+REDMINE="http://bericson:3000/repositories/revision/clientarchitecture/"
 TEMP_CI="gitcc_temp_checkin"
 
 def main(no_checkin=False):
@@ -121,7 +121,7 @@ def sendEmail(to,subject,content):
 
 def sendSummaryMessage(to,commit_id,lastCommitId):
     summary =  git_exec(['diff','--name-status','%s..%s' % (lastCommitId, commit_id)])
-    subject = "Your commit " + commit_id + " has been checked into clearcase";
+    subject = "Your commit " + commit_id + " has been checked into clearcase\n\nRedmine Link: " + REDMINE + commit_id;
     message = subject + "\n\n"
     message += summary
     try:
